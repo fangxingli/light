d5dac0683317fa75bf4b6f120682148ce6661a96
Lee <paul@paul-laptop.(none)>
2013-07-27 23:09:18 +0800

    q

--- src/com/light/MainActivity.java
+++ src/com/light/MainActivity.java
@@ -1,15 +1,25 @@
 package com.light;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+		
 		setContentView(R.layout.activity_main);
+		
+		Button up_button = (Button)findViewById(R.id.up_arrow);
+		up_button.setTypeface(font);
+		
+		Button down_button = (Button)findViewById(R.id.down_arrow);
+		down_button.setTypeface(font);
 	}
 
 	@Override
