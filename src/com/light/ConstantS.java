f9e2033e1a39c355e1817f6d46d7522f1b7fa750
lifangxing <ssdylfx@163.com>
2013-07-30 16:19:57 +0800

    使用新浪SDK

---
+++ src/com/light/ConstantS.java
@@ -0,0 +1,7 @@
+package com.light;
+
+public class ConstantS{
+	public static String APP_KEY="2040035480";
+	public static String REDIRECT_URL="https://hellotornado-grepi.rhcloud.com/success";
+	public static String SCOPE = "all";
+}
--- src/com/light/MainActivity.java
+++ src/com/light/MainActivity.java
@@ -1,25 +1,94 @@
 package com.light;
 
+import java.text.SimpleDateFormat;
+
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.weibo.sdk.android.Oauth2AccessToken;
+import com.weibo.sdk.android.Weibo;
+import com.weibo.sdk.android.WeiboAuthListener;
+import com.weibo.sdk.android.WeiboDialogError;
+import com.weibo.sdk.android.WeiboException;
 
 public class MainActivity extends Activity {
+	
+	private WebView	mWebView;
+	private Weibo mWeibo = null;
+	
+	public static Oauth2AccessToken accessToken;
+	
+	class AuthDialogListener implements WeiboAuthListener {
 
+        @Override
+        public void onComplete(Bundle values) {
+        	
+        	String code = values.getString("code");
+        	if(code != null){
+	        	Toast.makeText(MainActivity.this, "认证code成功" + code, Toast.LENGTH_SHORT).show();
+	        	return;
+        	}
+            String token = values.getString("access_token");
+            String expires_in = values.getString("expires_in");
+            MainActivity.accessToken = new Oauth2AccessToken(token, expires_in);
+            if (MainActivity.accessToken.isSessionValid()) {
+                String date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+                        .format(new java.util.Date(MainActivity.accessToken
+                                .getExpiresTime()));
+             
+                AccessTokenKeeper.keepAccessToken(MainActivity.this, accessToken); // 保存access_token
+                Toast.makeText(MainActivity.this, "认证成功", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+
+        @Override
+        public void onError(WeiboDialogError e) {
+            Toast.makeText(getApplicationContext(),
+                    "Auth error : " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(getApplicationContext(), "Auth cancel",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+            Toast.makeText(getApplicationContext(),
+                    "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+//		Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
 		
 		setContentView(R.layout.activity_main);
 		
-		Button up_button = (Button)findViewById(R.id.up_arrow);
-		up_button.setTypeface(font);
+		mWeibo = Weibo.getInstance(ConstantS.APP_KEY, ConstantS.REDIRECT_URL, ConstantS.SCOPE);
+		mWebView = (WebView) findViewById(R.id.WebView01);
 		
-		Button down_button = (Button)findViewById(R.id.down_arrow);
-		down_button.setTypeface(font);
+		
+		Button b = (Button)this.findViewById(R.id.sina_weibo);
+		b.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWeibo.anthorize(MainActivity.this, new AuthDialogListener());
+            }
+        });
+		
+		
 	}
 
 	@Override
